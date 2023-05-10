8275cfdd19f80d2e9d74703cf743a4b638de5f91
Ose <edmondumolu@hotmail.co.uk>
2023-05-09 23:56:09 -0700

    Setup to run react app

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
+import Homepage from './components/Homepage';
+import ChooseTemplate from './components/ChooseTemplate';
+import AdPrompt from './components/AdPrompt';
+import AdPromptSelected from './components/AdPromptSelected';
 import reportWebVitals from './reportWebVitals';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/choose-template" element={<ChooseTemplate />}></Route>
+        <Route path="/ad-prompt" element={<AdPrompt />}></Route>
+        <Route path="/ad-prompt-2" element={<AdPromptSelected />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
